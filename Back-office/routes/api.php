271ef427cefc55828e5caf0dcf00e6409a65a51f
Oltion-Koceku <koceku17@gmail.com>
2024-08-06 14:32:52 +0200

    [add] resoource route api

--- Back-office/routes/api.php
+++ Back-office/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\DaysController;
+use App\Http\Controllers\Api\StopsController;
+use App\Http\Controllers\Api\TripsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('days', DaysController::class);
+Route::resource('stops', StopsController::class);
+Route::resource('trips', TripsController::class);
+
